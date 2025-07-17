a52104311c8c31aa5df7df42e185a9e0c9da2b5b
chaiwatt <chaiwat_thaweechan@yahoo.com>
2025-07-18 06:49:13 +0700

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,11 +53,6 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
-        'temp' => [
-            'driver' => 'local',
-            'root' => storage_path('app/temp'),
-            'throw' => false,
-        ],
         'upload' => [
             'driver' => 'local',
             'root' => public_path('upload'), // previously storage_path();
